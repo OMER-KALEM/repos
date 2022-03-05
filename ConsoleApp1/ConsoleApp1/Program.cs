cdce4b79b528eb491e1918e2741afc3288014cdb
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2022-03-05 20:43:40 +0300

    Init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FinalNotu = 0;
+            int VizeNotu = 0;
+            int ProjeNotu = 0;
+
+
+
+            Console.Write("proje notunu gir");
+            ProjeNotu = Console.ReadLine();
+
+
+
+            Console.WriteLine(ProjeNotu);
+        }
+    }
+}
